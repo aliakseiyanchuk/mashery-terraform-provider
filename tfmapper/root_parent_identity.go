87bf1675f8542de68174ba94918ab0831fad9ea4
Aliaksei Yanchuk <aliaksei.yanchuk@hotmail.com>
2023-05-09 22:58:42 +0200

    Generified first resource: Mashery package.

--- tfmapper/root_parent_identity.go
+++ tfmapper/root_parent_identity.go
@@ -10,7 +10,7 @@ func (r RootParentIdentity) Identity(_ *schema.ResourceData) (Orphan, error) {
 	return 0, nil
 }
 
-func (r RootParentIdentity) Assign(ident Orphan, state *schema.ResourceData) error {
+func (r RootParentIdentity) Assign(_ Orphan, _ *schema.ResourceData) error {
 	// Nothing to do; ignored
 	return nil
 }
